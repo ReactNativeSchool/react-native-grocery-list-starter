f3636a6ba714df0895a914efe862ab61bf064bc3
Spencer Carli <spencercarli@gmail.com>
2019-05-07 10:44:27 -0500

    01 Project Setup

---
+++ App/data/nachos.js
@@ -0,0 +1,8 @@
+import uuid from "uuid/v4";
+
+export default [
+  { id: uuid(), name: "Beans" },
+  { id: uuid(), name: "Chips" },
+  { id: uuid(), name: "Cheese" },
+  { id: uuid(), name: "Habenero" }
+]
