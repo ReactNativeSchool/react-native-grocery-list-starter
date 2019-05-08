52b2f5734fe916c15f9b9c5233a43e296e1f364e
Spencer Carli <spencercarli@gmail.com>
2019-05-08 09:12:07 -0500

    06 Rendering Items with FlatList

--- App/screens/CurrentList.js
+++ App/screens/CurrentList.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import { View, Text, SafeAreaView, ScrollView } from 'react-native';
+import { View, Text, SafeAreaView, ScrollView, FlatList } from 'react-native';
 
 import nachos from '../data/nachos';
 
 import ListItem, { Separator } from '../components/ListItem';
 
 export default () => {
-
   return (
     <SafeAreaView>
-      <ScrollView>
-        {nachos.map((item, index) => (
-          <React.Fragment key={item.id}>
-            <ListItem
-              name={item.name}
-              onFavoritePress={() => alert('todo: handle favorite!')}
-              isFavorite={index < 2}
-            />
-            <Separator />
-          </React.Fragment>
-        ))}
-      </ScrollView>
+      <FlatList
+        data={nachos}
+        renderItem={({ item, index }) => (
+          <ListItem
+            name={item.name}
+            onFavoritePress={() => alert('todo: handle favorite!')}
+            isFavorite={index < 2}
+          />
+        )}
+        keyExtractor={(item) => item.id}
+        ItemSeparatorComponent={() => <Separator />}
+      />
     </SafeAreaView>
-  )
+  );
 };
