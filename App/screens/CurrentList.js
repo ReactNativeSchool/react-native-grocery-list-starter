d5516f8f7558921188f8687f20f5d7e3e7f9f5b6
Spencer Carli <spencercarli@gmail.com>
2019-05-08 09:32:31 -0500

    Capture User Input with TextInput

--- App/screens/CurrentList.js
+++ App/screens/CurrentList.js
@@ -1,25 +1,34 @@
 import React from 'react';
-import { View, Text, SafeAreaView, ScrollView, FlatList } from 'react-native';
+import { View, Text, SafeAreaView, ScrollView, FlatList, KeyboardAvoidingView } from 'react-native';
 
 import nachos from '../data/nachos';
 
 import ListItem, { Separator } from '../components/ListItem';
+import AddItem from '../components/AddItem';
 
 export default () => {
   return (
-    <SafeAreaView>
-      <FlatList
-        data={nachos}
-        renderItem={({ item, index }) => (
-          <ListItem
-            name={item.name}
-            onFavoritePress={() => alert('todo: handle favorite!')}
-            isFavorite={index < 2}
-          />
-        )}
-        keyExtractor={(item) => item.id}
-        ItemSeparatorComponent={() => <Separator />}
-      />
+    <SafeAreaView style={{ flex: 1 }}>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior="padding"
+      >
+        <FlatList
+          data={nachos}
+          renderItem={({ item, index }) => (
+            <ListItem
+              name={item.name}
+              onFavoritePress={() => alert('todo: handle favorite!')}
+              isFavorite={index < 2}
+            />
+          )}
+          keyExtractor={(item) => item.id}
+          ItemSeparatorComponent={() => <Separator />}
+          ListHeaderComponent={() => (
+            <AddItem />
+          )}
+        />
+      </KeyboardAvoidingView>
     </SafeAreaView>
   );
 };
