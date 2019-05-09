40f1998158acb2e673f5cbd91edcdfda3bf13265
Spencer Carli <spencercarli@gmail.com>
2019-05-09 09:20:02 -0500

    12 Persisting Lists between Sessions

--- App/screens/CurrentList.js
+++ App/screens/CurrentList.js
@@ -1,23 +1,49 @@
-import React, { useState } from 'react';
-import { View, Text, SafeAreaView, ScrollView, FlatList, KeyboardAvoidingView } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, SafeAreaView, ScrollView, FlatList, KeyboardAvoidingView, ActivityIndicator } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
 import uuid from "uuid/v4";
-
-import nachos from '../data/nachos';
 
 import ListItem, { Separator } from '../components/ListItem';
 import AddItem from '../components/AddItem';
 
+const updateStoredCurrentList = (list) => {
+  AsyncStorage.setItem('@@GroceryList/currentList', JSON.stringify(list));
+};
+
 export default () => {
-  const [list, setList] = useState(nachos);
+  const [list, setList] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   const addItem = (text) => {
-    setList([{ id: uuid(), name: text}, ...list]);
+    const newList = [{ id: uuid(), name: text}, ...list];
+    setList(newList);
+    updateStoredCurrentList(newList);
   };
 
   const removeItem = (id) => {
     const newList = list.filter(item => item.id !== id);
     setList(newList);
+    updateStoredCurrentList(newList);
   };
+
+  useEffect(() => {
+    AsyncStorage.getItem('@@GroceryList/currentList')
+      .then(data => JSON.parse(data))
+      .then(data => {
+        if (data) {
+          setList(data);
+        }
+        setLoading(false);
+      })
+  }, []);
+
+  if (loading) {
+    return (
+      <SafeAreaView>
+        <ActivityIndicator size="large" />
+      </SafeAreaView>
+    );
+  }
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
